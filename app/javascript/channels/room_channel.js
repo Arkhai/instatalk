a60e2d7f68e97c0e4ea68d89a2e86bd636e2052f
arkhai <kiriam@bk.ru>
2022-03-30 20:52:28 +0300

    Chanel for online check added

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -1,19 +1,33 @@
 import consumer from "./consumer"
 
-consumer.subscriptions.create("RoomChannel", {
+jQuery(document).on 'turbolinks:load', ->
+messages = $('#messages')
+
+if messages.length > 0
+  createRoomChannel messages.data('room-id')
+
+$(document).on 'keypress', '#message_body', (event) ->
+    message = event.target.value
+if event.keyCode is 13 && message != ''
+App.room.speak(message)
+event.target.value = ""
+event.preventDefault()
+
+consumer.subscriptions.create({"RoomChannel", roomId: roomId },{
   connected() {
-    // Called when the subscription is ready for use on the server
+    console.log('Connected to RoomChannel')
   },
 
   disconnected() {
-    // Called when the subscription has been terminated by the server
+    console.log('Disconnected from RoomChannel')
   },
 
   received(data) {
-    // Called when there's incoming data on the websocket for this channel
+    console.log('Received message: ' + data['message'])
+    $('#messages').append data['message']
   },
 
   speak: function() {
-    return this.perform('speak');
+    return this.perform('speak', { message: message });
   }
 });
